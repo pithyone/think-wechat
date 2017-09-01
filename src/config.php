9cd712ddb270b221d7c03ee763c628350553a01a
王兵 <pithyone@vip.qq.com>
2017-09-01 17:38:13 +0800

    Apply fixes from StyleCI (#1)
    [ci skip] [skip ci]

--- src/config.php
+++ src/config.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    /**
+    /*
      * 日志配置
      */
     'log'      => [
         'handler' => new \think\LoggerBridge(),
     ],
 
-    /**
+    /*
      * 企业唯一ID，可在管理后台“我的企业”－“企业信息”下查看（需要有管理员权限）
      */
     'corp_id'  => 'your-corp-id',
 
-    /**
+    /*
      * 通讯录同步助手配置，可以对部门和成员进行查询、添加、修改、删除操作（其他应用无操作权限）
      *
      * token：   可任意填写，用于生成签名
@@ -23,10 +23,10 @@ return [
     'contacts' => [
         'token'   => 'your-contacts-agent-token',
         'aes_key' => 'your-contacts-agent-aes-key',
-        'secret'  => 'your-contacts-agent-secret'
+        'secret'  => 'your-contacts-agent-secret',
     ],
 
-    /**
+    /*
      * 其他应用配置，注意：需要使用的配置都需要按照如下格式进行添加
      *
      * test：    应用标识，可随意指定（不重复）
@@ -39,10 +39,10 @@ return [
         'agent_id' => 'your-test-agent-id',
         'token'    => 'your-test-agent-token',
         'aes_key'  => 'your-test-agent-aes-key',
-        'secret'   => 'your-test-agent-secret'
+        'secret'   => 'your-test-agent-secret',
     ],
 
-    /**
+    /*
      * 缓存配置
      */
     'cache'    => new \think\CacheBridge(),
